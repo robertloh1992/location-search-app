baa833b9bc47bc06ae615a658489df2e4de7e1e6
Robert Loh <robert@Roberts-MacBook-Air.local>
2024-11-24 22:50:30 +0800

    add prettier config file and gitignore config file

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    printWidth: 100,
+    semi: false,
+    tabWidth: 2,
+    useTabs: false,
+    singleQuote: true,
+    trailingComma: 'es5',
+    bracketSpacing: true,
+    bracketSameLine: false,
+    arrowParens: 'avoid',
+}
+  
